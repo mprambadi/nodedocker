9ffe1b7d1652ffcc976fd6a3cb8d7e014f7c4e5e
Maulana <mprambadi@gmail.com>
2020-03-16 15:44:16 +0700

    add port env

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
   res.json({
     hello: "world",
     lagi: "lagilagi",
-    test:"test",
-    baru:"lagi"
+    test: "test",
+    baru: "lagi",
+    port
   });
 });
 
-app.listen(3000, () => {
-  console.log("app nya jalan");
+app.listen(port, () => {
+  console.log(`app nya jalan ${port}`);
 });
